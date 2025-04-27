189df13fcc0ea1bc78812023fb6e0cbeb6e6e64d
sandeepj <sandeepdjadhav24@outlook.com>
2025-04-27 15:41:09 +0530

    3

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.25.0"
+      }
+    }
+}
+
+provider azurerm {
+    features {}
+    subscription_id = "ec49edde-0e94-4197-abdd-1d8d13b05b62"
+}
+
+resource "azurerm_resource_group" "sandy" {
+    name = "sandy2255"
+    location = "eastus"  
+}
